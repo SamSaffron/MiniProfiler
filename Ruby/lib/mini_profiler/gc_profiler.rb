4fe23c27051631157cde1c0016652f48f785d5e9
Nathan Youngman <git@nathany.com>
2013-08-29 16:57:30 -0600

    include memsize for gc profiler

--- Ruby/lib/mini_profiler/gc_profiler.rb
+++ Ruby/lib/mini_profiler/gc_profiler.rb
@@ -1,11 +1,17 @@
+require 'objspace'
+
 class Rack::MiniProfiler::GCProfiler
 
+  ObjectStats = Struct.new(:objects, :bytes)
+
   def object_space_stats
-    stats = Hash.new(0)
+    stats = {}
     ids = Set.new
     ObjectSpace.each_object { |o|
       begin
-        stats[o.class] += 1
+        stats[o.class] ||= ObjectStats.new(0,0)
+        stats[o.class].objects += 1
+        stats[o.class].bytes += ObjectSpace.memsize_of(o)
         # store object ids for string analysis:
         ids << o.object_id if Integer === o.object_id
       rescue NoMethodError
@@ -16,13 +22,21 @@ class Rack::MiniProfiler::GCProfiler
     {:stats => stats, :ids => ids}
   end
 
-  def diff_object_stats(before,after)
+  def diff_object_stats(before, after)
     diff = {}
-    after.each do |k,v|
-      diff[k] = v - (before[k] || 0)
+    after.each do |klass,v|
+      diff[klass] = v.dup
+      if before.has_key?(klass)
+        diff[klass].objects -= before[klass].objects
+        diff[klass].bytes -= before[klass].bytes
+      end
     end
-    before.each do |k,v|
-      diff[k] = 0 - v unless after[k]
+    before.each do |klass,v|
+      unless after[klass].objects
+        diff[klass] = ObjectStats.new(0,0)
+        diff[klass].objects = 0 - v.objects
+        diff[klass].bytes = 0 - v.bytes
+      end
     end
 
     diff
@@ -67,38 +81,36 @@ class Rack::MiniProfiler::GCProfiler
       b = app.call(env)[2]
       b.close if b.respond_to? :close
       stat_after = object_space_stats
-      
+
       diff = diff_object_stats(stat_before[:stats],stat_after[:stats])
       string_analysis = analyze_strings(stat_before[:ids], stat_after[:ids])
     ensure
       GC.enable
     end
 
+    line = ('---' * 20) << "\n\n"
 
-    body << "
-ObjectSpace delta caused by request:
---------------------------------------------\n"
-    diff.to_a.reject{|k,v| v == 0}.sort{|x,y| y[1] <=> x[1]}.each do |k,v|
-      body << "#{k} : #{v}\n" if v != 0
+    body << "ObjectSpace delta caused by request:\n" << line
+    diff.to_a.reject{|k,v| v.objects == 0}.sort{|x,y| y[1].bytes <=> x[1].bytes}.each do |k,v|
+      body << output_object_space(k,v)
     end
 
-    body << "\n
-ObjectSpace stats:
------------------\n"
-
-    stat_after[:stats].to_a.sort{|x,y| y[1] <=> x[1]}.each do |k,v|
-      body << "#{k} : #{v}\n" 
+    body << "\nObjectSpace stats:\n" << line
+    stat_after[:stats].to_a.sort{|x,y| y[1].bytes <=> x[1].bytes}.each do |k,v|
+      body << output_object_space(k,v)
     end
 
-
-    body << "\n
-String stats:
-------------\n"
-
+    body << "\nString stats:\n" << line
     string_analysis.to_a.sort{|x,y| y[1] <=> x[1] }.take(1000).each do |string,count|
       body << "#{count} : #{string}\n"
     end
 
     return [200, {'Content-Type' => 'text/plain'}, body]
+  end
+
+  private
+
+  def output_object_space(klass, stats)
+    "#{klass} : #{stats.objects} : #{(stats.bytes/1024).floor}K\n"
   end
 end
